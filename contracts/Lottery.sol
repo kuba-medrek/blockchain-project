2fda31c1fb0e5985b66dd4ecca733643686655c0
wojtek <wookl006@o2.pl>
2019-07-16 05:03:39 -0700

    Updated lottery system

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,51 +3,68 @@ pragma solidity >=0.4.21 <0.6.0;
 // 1 ether = 1000000000000000000 wei
 
 contract Lottery {
-	mapping (address => uint) public players_inputs;
 	address[] public players;
 	address public owner;
+	uint public tickets;
+	uint public ticket_price;
+	address[] public winners;
 	enum LotteryStatus {Open, Closed}
 	LotteryStatus state;
 
     constructor() public {
         owner = msg.sender;
+        tickets = 15;
         state = LotteryStatus.Open;
+        ticket_price = 1 ether;
 	}
 
 	function getNumberOfPlayers() public returns(uint256) {
 		return players.length;
 	}
 
-	function getBalance() public returns(uint256) {
-		address(this).balance;
+	function getBalance() public view returns(uint256) {
+		return address(this).balance;
 	}
 
-	function play() public payable {
-		require(msg.value > 0.001 ether);
+	function play(uint tickets_bought) public payable returns(bool) {
 		require(state == LotteryStatus.Open);
+		require(tickets_bought <= tickets);
 
-		players_inputs[msg.sender] = msg.value;
-		players.push(msg.sender);
+		tickets -= tickets_bought;
+
+		for(uint i=0; i < tickets_bought; ++i){
+			players.push(msg.sender);
+		}
+
+		if(tickets == 0) chooseWinner();
+		return true;
 	}
 
 	function chooseWinner() public {
-		require(msg.sender == owner);
-		require(getBalance() > 1.00 ether);
+		state = LotteryStatus.Closed;
+		uint winner_number = random(players.length);
+		address payable winner = make_payable(players[winner_number]);
+		winners.push(players[winner_number]);
+		winner.transfer(getBalance());
+		restartLottery();
 	}
 
-	function cleanMapping() internal {
-		for(uint i=0; i < players.length; ++i){
-			players_inputs[players[i]] = 0;
-		}
+	function restartLottery() internal {
 		players.length = 0;
+		tickets = 15;
+		state = LotteryStatus.Open;
 	}
 
-	function boom(address payable recipient) public {
+	function destroy_contract(address payable recipient) public {
 		require(msg.sender == owner);
 		selfdestruct(recipient);
 	}
 
-	function random(uint scope) private view returns (uint) {
+	function random(uint scope) public view returns (uint) {
 		return uint(keccak256(abi.encodePacked(now, block.difficulty, players))) % scope;
  	}
+
+ 	function make_payable(address x) internal pure returns (address payable) {
+      return address(uint160(x));
+  	}
 }
